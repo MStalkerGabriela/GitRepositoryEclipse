f7aab6791d1066cec527a1814d027b190d4afca7
MStalkerGabriela <mstalker@insgabrielamistral.cat>
2025-02-13 11:14:55 +0100

    Update MainClass.java

--- src/eclipse_Git/MainClass.java
+++ src/eclipse_Git/MainClass.java
@@ -1,12 +1,13 @@
 package eclipse_Git;
 
 public class MainClass {
-public static void main (String[] args) {
+	public static void main (String[] args) {
 		
 		System.out.println("Hola mundo que tal?"); 
 		
 		System.out.println("1a modificació Git");
-		
+
+		System.out.println(“2a modificació GitHub”);
 	}
 
 }
